fd8b97994c6f24855f9e842f41c7c6e93202bee8
sigma student <mohammedasifpasha908@gmail.com>
2026-07-19 13:04:31 +0530

    Handle Multiple Inputs

--- Learning/src/Form.jsx
+++ Learning/src/Form.jsx
@@ -1,16 +1,46 @@
+//handle multiple Inputs
 import {useState} from 'react';
 export default function Form() {
-    let [fullName, setFullName] = useState("Asif");
+    let [formData, setFormData] = useState({
+        fullName : "",
+        password : "",
+    });
 
-    let handleChange = (event) => {
-        setFullName(event.target.value);
+    let handleInputChange = (event) => {
+        setFormData((currData) => {
+            return {...currData, [event.target.name] : event.target.value} ;
+        });
+
+
+    //    let fieldName = event.target.name;
+      //  let newValue = event.target.value;
+//
+  //      setFormData((currData) => {
+    //        currData[fieldName] = newValue;
+        //    return {...currData};
+       // });
+    };
+
+    let hanldeSubmit = (event) =>{
+        event.preventDefault();
+        console.log(formData);
+        setFormData({
+             fullName : "",
+             password : "",
+        });
     };
     return(
-        <form>
-            <label htmlFor='username' > Full Name: </label>
-            <input placeholder='enter full name' id='username'
-            type="text" value={fullName}
-            onChange={handleChange} />
+        <form onSubmit={hanldeSubmit}>
+            <label htmlFor='fullName' > Full Name: </label>
+            <input placeholder='enter full name' id='fullName'
+            type="text" value={formData.fullName} name='fullName'
+            onChange={handleInputChange} />
+            <br></br>
+            <br></br>
+            <label htmlFor='password'>Password : </label>
+            <input placeholder='enter Password' type='password'
+            id='password' name='password' value={formData.password}
+            onChange={handleInputChange}/>
             <button>Submit</button>
         </form>
         
